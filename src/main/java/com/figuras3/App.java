1cb3cf58fa441ff28dd4e8d493946cebc9994005
m5hugo <sinacabar@gmail.com>
2019-01-30 11:00:49 +0100

    modificado main para escribir area del circulo

--- src/main/java/com/figuras3/App.java
+++ src/main/java/com/figuras3/App.java
@@ -8,9 +8,17 @@ public final class App {
     /**
      * @param args The arguments of the program.
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
     	
-        System.out.println("Hello World!");
+        System.out.println("-------------- Hello World!-----------------\n\n");
+        
+        try {
+            Circulo circulo = new Circulo(3);
+            System.out.println("Area Circulo de radio " + circulo.getRadi() + ": " + circulo.areaCirculo());
+        }
+        catch (Exception e) {
+             System.out.println(e.getMessage());
+        }
     }
 }
 
