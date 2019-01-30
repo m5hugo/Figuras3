d7472bb51aab6b1aaf7330e24116fa0c378f1647
m5hugo <sinacabar@gmail.com>
2019-01-30 09:40:38 +0100

    first commit

---
+++ src/main/java/com/figuras3/App.java
@@ -0,0 +1,16 @@
+package com.figuras3;
+
+
+public final class App {
+    private App() {
+    }
+
+    /**
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+    	
+        System.out.println("Hello World!");
+    }
+}
+
---
+++ src/main/java/com/figuras3/Circulo.java
@@ -0,0 +1,46 @@
+package com.figuras3;
+
+public class Circulo 
+{
+	private int radi;
+	private static final double PI = Math.PI;
+
+	public Circulo(int radi) throws Exception
+	{
+		if ((radi <=0) || ((radi % 1) != 0))
+		{
+			throw new Exception("El radi ha de ser un enter > ó = a 0");
+		}
+		else
+		{
+			this.radi = radi;
+		}
+	}
+
+	public double areaCirculo()
+	{
+		return Math.pow(this.radi, 2)*PI;
+	}
+	
+	public double perimetroCirculo()
+	{
+		return 2*PI*this.radi;
+	}
+	
+	
+	public int getRadi() {
+		return radi;
+	}
+
+	public void setRadi(int radi) throws Exception {
+
+		if ((radi <=0) || ((radi % 1) != 0))
+		{
+			throw new Exception("El radi ha de ser un enter > ó = a 0");
+		}
+		else
+		{
+			this.radi = radi;
+		}
+	}
+}
